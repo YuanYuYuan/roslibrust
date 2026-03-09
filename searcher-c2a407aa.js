1568fefa016ee69a50b99e275ed0f00f521d8dd9
YuanYuYuan <YuanYuYuan@users.noreply.github.com>
2026-03-09 17:03:03 +0000

    deploy: 37c93ea11c4855d615e9430ee2de3d20d8f177d3

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-844379ed.js',
+                path_to_root + 'searchindex-da852613.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
